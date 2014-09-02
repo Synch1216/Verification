51aa3120afddb8bcc7c3b654b851d3342d42d2ce
Synch1216 <malcolmbarnes@Malcolm-Barness-MacBook-Pro.local>
2014-09-02 05:10:13 -0400

    verifications

---
+++ Verifications.R
@@ -0,0 +1,10 @@
+# What version of R and R Studio are you using?
+<R.Version()>
+#R Version 3.1.1 (2014-07-10)
+# What version of PostgreSQL do you have installed?
+<ls>
+#I have postgreSQL-9.3.5-1-osz.app
+# How many observations are in sales?
+<show(sales)>
+  
+# There are 401,146 observations. 
