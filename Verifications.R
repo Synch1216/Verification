6e0bca4798cd0167c546bdee16f236aab8fb60f6
Synch1216 <malcolmbarnes@Malcolm-Barness-MacBook-Pro.local>
2014-09-02 05:21:57 -0400

    Verifications

--- Verifications.R
+++ Verifications.R
@@ -1,6 +1,6 @@
 # What version of R and R Studio are you using?
 <R.Version()>
-#R Version 3.1.1 (2014-07-10)
+#R Version 3.1.1 (2014-07-10), RStudio Version 0.98.1028
 # What version of PostgreSQL do you have installed?
 <ls>
 #I have postgreSQL-9.3.5-1-osz.app
