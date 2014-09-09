7bfb8919844779c67aa54a3c6d50510cff1ab63f
Synch1216 <malcolmbarnes@Malcolm-Barness-MacBook-Pro.local>
2014-09-09 16:15:32 -0400

    Vector

---
+++ Vector.R
@@ -0,0 +1,16 @@
+#1. Assign five individuals to a vector called queue:
+> queue <-c("James","Mary","Steve","Alex", "Patricia")
+#> queue
+#[1] "James"    "Mary"     "Steve"    "Alex"     "Patricia"
+#2. Update queue to reflect arrival of Harold.
+x <-list(queue)
+#x[["Harold"]] <- 6
+#3. Reflect the queue as if James has finished.
+#> class(x[1,1, drop])
+#4. Patricia talks her way in front of Steve:
+#>queue <-c("Mary","Patricia","Steve", "Alex","Harold")
+#5. Harold grows impatient and leaves:
+#>queue <-c("Mary","Patricia","Steve", "Alex")
+#6. rm[["Alex"]]
+#7.list[1,2]
+#8. nrow(x)
