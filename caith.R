5413cbfb2ea49660dfede28010c28cf75a068415
Synch1216 <malcolmbarnes@Malcolm-Barness-MacBook-Pro.local>
2014-10-04 00:01:28 -0400

    o

--- caith.R
+++ caith.R
@@ -6,4 +6,18 @@ Require(MASS)
 data(caith)
 EyeColor <-rowSums(caith)
 EyeColor
-#2. The rows blue, light, medium and dark and their totals across the variables are here. 
+#2. The rows blue, light, medium and dark and their totals across the variables are here.
+#3:
+hairColor <- colSums(caith)
+hairColor
+#4. Medium is the most common eye color.
+hist(EyeColor)
+#5. This reassigns the dataset caith as a matrix named caitha.
+caitha <- as.matrix(caith)
+caitha
+#6. This creates a plot for each eye color and then subdivides the bars to illustrate the variables of hair color.
+barplot(t(as.matrix(caith)), beside=TRUE)
+#7.
+barplot(t(as.matrix(caith)), beside=TRUE)
+#8. There is a correlation between medium and dark eyes being prominent on medium and darker hair colors.
+barplot(t(as.matrix(caith)), beside=TRUE)
