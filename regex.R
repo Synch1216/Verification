92aa0ea2a78e1fb2c7a7bb74ac3b128247c876b2
Synch1216 <malcolmbarnes@Malcolm-Barness-MacBook-Pro.local>
2014-10-13 14:33:19 -0400

    5

---
+++ regex.R
@@ -0,0 +1,9 @@
+# 1. Regex may be useful when analyzing documents with dates and times in different forms. A string function would be vulnerable to error. The Regex would provide a number of different format options to match in the documents.
+
+#2. RMarkdown documents code and makes it easy for someone to come up with the exact results that you did at a certain time instead of trying to figure out what you did. The notes come in handy because they also allow comparisons between intention and results. Rmarkdown consolidates all of the information needed to investigate an error.
+
+#3. Correlation does not imply causation. What this means is that even though two facts may occur at the same time- one may not cause the other. For example just because it happens to rain after a tribe does a ceremonial dance does not mean that the dance caused the rain.
+
+#4. Variable coding is great fr when you need to use the result of a previous operation in a new calculation. 
+# Example: total <- data1 + data3
+#avg <- total/2
