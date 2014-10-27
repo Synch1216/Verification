6e2165707c5365281c633f2b7384f92f1ab7e210
Synch1216 <malcolmbarnes@Malcolm-Barness-MacBook-Pro.local>
2014-10-26 22:32:45 -0400

    hh

---
+++ week9quiz.R
@@ -0,0 +1,7 @@
+#1. Web scraping is usually performed with HTML and is less structured. Web API is associated with XML and JSON and is more structured.
+
+#2. Web crawling is ubiquitous in the sense that a large portion of web pages are indiscriminately indexed while web scraping is a more active pull of queries from web pages.
+
+#3. The robots.txt file issues instructions about the site to web robots (disallowing them from visiting its pages).
+
+#4. R is adequate for scraping web pages but may be edged by Python.
