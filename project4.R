5129df92920fba169871cdb2ae659cb9a6751b91
Synch1216 <malcolmbarnes@Malcolm-Barness-MacBook-Pro.local>
2014-10-28 23:45:08 -0400

    h

--- project4.R
+++ project4.R
@@ -3,8 +3,7 @@ devtools::install_github("hadley/rvest")
 library(rvest)
 hobbit <- html("http://www.rottentomatoes.com/m/the_hobbit_the_desolation_of_smaug/reviews/")
 score <- hobbit %>% 
-  html_nodes("span.tMeterScore") %>%
-  html_text() %>%
-  as.numeric()
+  html_nodes(".tMeterScore") %>%
+  html_text()
 score
-
+# Pulled the ratings of The Hobbit movie from Rotten Tomatoes.
