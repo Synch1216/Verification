21ba5b90b449b7dc4a40dd8f33983f93738b0919
Synch1216 <malcolmbarnes@Malcolm-Barness-MacBook-Pro.local>
2014-11-11 23:37:38 -0500

    msg

---
+++ sign11.sql
@@ -0,0 +1,16 @@
+﻿SELECT 
+  weather.year, 
+  weather.month, 
+  weather.day, 
+  weather.hour, 
+  weather.wind_dir
+  (select case
+when wind_dir <=90 then 'North'
+when wind_dir > 90 then and wind_dir <=180 then 'East'
+when wind_dir > 180 then and wind_dir <= 270 then 'South' 
+else 'West'
+end as the_wind_direction)
+FROM 
+  public.weather
+  where day=1 and hour=1
+order by month;
