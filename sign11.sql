874267033601e59e078974ac0fb377b2980a4533
Synch1216 <malcolmbarnes@Malcolm-Barness-MacBook-Pro.local>
2014-11-12 08:14:13 -0500

    O

--- sign11.sql
+++ sign11.sql
@@ -8,7 +8,8 @@
 when wind_dir <=90 then 'North'
 when wind_dir > 90 and wind_dir <=180 then 'East'
 when wind_dir > 180 and wind_dir <= 270 then 'South' 
-else 'West'
+when wind_dir > 270 and wind_dir <= 360 then 'West' 
+else '?'
 end as the_wind_direction)
 FROM 
   public.weather
