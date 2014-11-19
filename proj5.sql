839b085344729592fbcb8f62028f48099c1b6c27
Synch1216 <malcolmbarnes@Malcolm-Barness-MacBook-Pro.local>
2014-11-18 23:56:22 -0500

    u

--- proj5.sql
+++ proj5.sql
@@ -1,1 +1,19 @@
-﻿select
+﻿-- 1. Weather associated with delays?
+select
+f.tailnum,
+f.dep_delay,
+w.pressure,
+w.wind_speed,
+w.precip,
+w.visib
+from flights f 
+join weather w
+on f.year = w.year
+AND f.month = w.month
+AND f.day = w.day
+where f.origin in ('LGA', 'JFK')
+order by f.dep_delay desc;
+
+2. 
+
+
