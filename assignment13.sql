026802f84f7d62c63bb0856cf3a9cb3f2251b58e
Synch1216 <malcolmbarnes@Malcolm-Barness-MacBook-Pro.local>
2014-11-25 23:28:24 -0500

    y

---
+++ assignment13.sql
@@ -0,0 +1,5 @@
+﻿-- Every airport and airline name in the flights database.
+select a.name, i.name from airlines a, airports i order by airlines, airports
+
+-- Every airports north of Fairbanks: 
+ 
