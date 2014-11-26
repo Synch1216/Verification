92f5002a7c799ea5c9b51e0c49a0cf2009ddc300
Synch1216 <malcolmbarnes@Malcolm-Barness-MacBook-Pro.local>
2014-11-25 23:59:26 -0500

    q

--- assignment13.sql
+++ assignment13.sql
@@ -2,4 +2,11 @@
 select a.name, i.name from airlines a, airports i order by a , i;
 
 -- Every airports north of Fairbanks: 
+with max_air as ( select name, lat from airports where lat >
+( select lat from airports where name like 'Fairbanks%')
+)
+select a.name , a.lat , b.name, b.lat
+from max_air a, max_air b
+where a.lat> b.lat
+order by a.name, b.lat desc;
  
